a5bf7a2dab1ecde6132afaee10038352634947cf
Zac <a19860101@gmail.com>
2024-05-29 16:21:09 +0800

    20240529

--- 0529/vuex/store.js
+++ 0529/vuex/store.js
@@ -1,11 +1,17 @@
 let store = new Vuex.Store({
     state:{
         title:'hello vuex',
-        count: 999
+        count: 999,
+        products:['11','22','33'],
     },
     mutations:{
         add(state,payload){
             state.count += payload;
+        }
+    },
+    getters:{
+        test(state){
+            return state.products.filter(data=>data === '11');
         }
     }
 });
